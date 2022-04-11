57d7f60dbf965cc71b71a559b6f5664305da95ac
OrgesaQorrolli <94568710+OrgesaQorrolli@users.noreply.github.com>
2022-04-11 21:41:00 +0200

    Update SServer.cs
    some updates

--- SServer.cs
+++ SServer.cs
@@ -12,5 +12,30 @@ namespace SServer
     {
         static void Main(string[] args)
         {
-   
-   
+            Console.WriteLine("Server is waiting for connections...");
+            UdpClient client = new UdpClient(8080);
+
+            IPEndPoint remoteip = new IPEndPoint(IPAddress.Any, 8080);
+
+            byte[] receivedbytes = client.Receive(ref remoteip);
+
+
+            if (receivedbytes != null)
+            {
+
+                string message = Encoding.ASCII.GetString(receivedbytes);
+                Console.WriteLine("received message " + message);
+
+            }
+
+
+            else
+            {
+                Console.WriteLine("Empty message received!");
+            }
+
+                Console.ReadLine();
+
+
+            }
+        }}
