87dd6c39a71afe444497fd56ecc0a60856987185
OrgesaQorrolli <94568710+OrgesaQorrolli@users.noreply.github.com>
2022-04-11 22:04:14 +0200

    Update client.cs

--- client.cs
+++ client.cs
@@ -1,1 +1,35 @@
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System;
 
+public class EchoClient
+{
+    public static void Main()
+    {
+        try
+        {
+            TcpClient client = new TcpClient("127.0.0.1", 8080);
+            StreamReader reader = new StreamReader(client.GetStream());
+            StreamWriter writer = new StreamWriter(client.GetStream());
+            String s = String.Empty;
+            while (!s.Equals("Exit"))
+            {
+                Console.Write("Futni një string për ta dërguar te serveri: ");
+                s = Console.ReadLine();
+                Console.WriteLine();
+                writer.WriteLine(s);
+                writer.Flush();
+                String server_string = reader.ReadLine();
+                Console.WriteLine(server_string);
+            }
+            reader.Close();
+            writer.Close();
+            client.Close();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+    } 
+    }
