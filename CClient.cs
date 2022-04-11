bbaf53aa1bfe4917f77ec5d2b8dc46b54749bcbd
OrgesaQorrolli <94568710+OrgesaQorrolli@users.noreply.github.com>
2022-04-11 21:56:20 +0200

    Update CClient.cs
    final test

--- CClient.cs
+++ CClient.cs
@@ -1,1 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
 
+
+
+namespace UDPClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UdpClient client = new UdpClient();
+            client.Connect(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8080));
+
+            Console.Write(">");
+            string inputs = Console.ReadLine();
+            if (inputs != null)
+            {
+                byte[] bytesent = Encoding.ASCII.GetBytes(inputs);
+
+                client.Send(bytesent, bytesent.Length);
+                Console.WriteLine("Mesazhi u dërgua me sukses!");
+
+                client.Close();
+                Console.ReadLine();
+            }
+}
+}}
